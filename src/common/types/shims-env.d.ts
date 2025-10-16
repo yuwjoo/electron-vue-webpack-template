980f943d3870671334d439d507ef182cc23dac4c
Hui Yang <1191759350@qq.com>
2025-10-16 22:19:57 +0800

    添加框架窗口名称环境变量类型

--- src/common/types/shims-env.d.ts
+++ src/common/types/shims-env.d.ts
@@ -2,5 +2,6 @@ declare namespace NodeJS {
   interface ProcessEnv {
     NODE_ENV: "development" | "production";
     MAIN_WINDOW_NAME: string; // 主窗口名称
+    FRAME_WINDOW_NAME: string; // 框架窗口名称
   }
 }
