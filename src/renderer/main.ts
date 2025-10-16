9a83d7b8b82172d5562df0fad5c73396f6a9fb89
Hui Yang <1191759350@qq.com>
2025-10-16 23:48:49 +0800

    添加vue devtoolsc插件

--- src/renderer/main.ts
+++ src/renderer/main.ts
@@ -5,6 +5,11 @@ import { createPinia } from "pinia";
 
 import App from "./App.vue";
 import router from "./router";
+import { devtools } from "@vue/devtools";
+
+if (process.env.NODE_ENV === "development") {
+  devtools.connect("http://localhost", 8090);
+}
 
 const app = createApp(App);
 
